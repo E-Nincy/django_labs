3827e332afdbd25cf16560416f10ce0125215d0b
E-Nincy <escotonincy10@outlook.es>
2025-08-13 15:24:54 +0800

    fixed_missing_data

--- 05_blog_overview/01_fix_missing_data_through_the_django_shell/learning_log/urls.py
+++ 05_blog_overview/01_fix_missing_data_through_the_django_shell/learning_log/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
+from learning_logs import views
 
 urlpatterns = [
-    path('', include('learning_logs.urls')),
+    path('admin/', admin.site.urls), 
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
 ]

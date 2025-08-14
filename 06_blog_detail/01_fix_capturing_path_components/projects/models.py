43e1dc36fb57b81fb9aba22d2c5debbd0936b9df
E-Nincy <escotonincy10@outlook.es>
2025-08-14 18:53:37 +0800

    fixed_capturing_path

--- 06_blog_detail/01_fix_capturing_path_components/projects/models.py
+++ 06_blog_detail/01_fix_capturing_path_components/projects/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=255, blank=True)
     description = models.TextField()
     technology = models.CharField(max_length=20)
--- 06_blog_detail/01_fix_capturing_path_components/projects/views.py
+++ 06_blog_detail/01_fix_capturing_path_components/projects/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
-
 def index(request):
-    projects = Project.objects.all()
-    return render(request, 'projects/index.html', {'projects': projects})
+    projects_list = Project.objects.all().order_by('-id')
+    context = {'projects': projects_list}
+    return render(request, 'projects/index.html', context)
 
-
-def detail(request):
-    project = Project.objects.get(pk=100)
-    return render(request, 'projects/detail.html', {'project': project})
+def detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    context = {'project': project}
+    return render(request, 'projects/detail.html', context)
